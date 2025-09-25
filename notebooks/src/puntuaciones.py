1f3f14404ec9376a8ec26c0260d90a29790ceff1
Abril Reigenborn <abrilreigenborn@gmail.com>
2025-09-25 12:01:14 -0300

    Funcion que inicializa los acumulados

--- notebooks/src/puntuaciones.py
+++ notebooks/src/puntuaciones.py
@@ -5,3 +5,13 @@ def mejor_equipo_ronda(ronda):
     puntajes = {equipo: puntos_por_equipo(datos_equipo) for equipo, datos_equipo in ronda.items()}
     mejor_equipo = max(puntajes, key=puntajes.get)
     return mejor_equipo, puntajes[mejor_equipo]
+
+def inicializar_acumulados(lista_equipos):
+    return {
+        equipo: {"innovacion": 0,
+                 "presentacion": 0,
+                 "errores": 0,
+                 "mejores": 0,
+                 "puntos": 0}
+        for equipo in lista_equipos
+    }
